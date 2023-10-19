f4bb0e6113abe9de651e8423d9cc1daf3ae213f3
John Galt <johngaltfirstrun@gmail.com>
2023-10-19 12:19:55 -0400

    clocks: import SysUI plugins from Husky and Barbet
    Reference: https://github.com/sarthakroy2002/vendor_SystemUIClocks

---
+++ clocks.mk
@@ -0,0 +1,11 @@
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/themes
+
+PRODUCT_PACKAGES += \
+    SystemUIClocks-BigNum \
+    SystemUIClocks-Calligraphy \
+    SystemUIClocks-Flex \
+    SystemUIClocks-Growth \
+    SystemUIClocks-Handwritten \
+    SystemUIClocks-Inflate \
+    SystemUIClocks-NumOverlap
--- common.mk
+++ common.mk
@@ -1,3 +1,8 @@
 # Fonts
 include vendor/themes/fonts.mk
+
+# Dark overlay
 include vendor/themes/system.mk
+
+# Clocks
+include vendor/themes/clocks.mk
