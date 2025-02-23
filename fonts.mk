94026287aad2fb394b7028a0ce5f3e35b6bafabd
Ido Ben-Hur <idoybh2@gmail.com>
2025-02-23 21:19:06 +0200

    fonts: Remove useless PRODUCT_PACKAGES additions

--- fonts.mk
+++ fonts.mk
@@ -1,6 +1,6 @@
 # Fonts
 PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*,vendor/themes/fonts//prebuilt,$(TARGET_COPY_OUT_PRODUCT)/fonts) \
+    $(call find-copy-subdir-files,*,vendor/themes/fonts/prebuilt,$(TARGET_COPY_OUT_PRODUCT)/fonts) \
     vendor/themes/fonts/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml
 
 PRODUCT_PACKAGES += \
@@ -13,56 +13,3 @@ PRODUCT_PACKAGES += \
     FontOneplusSlateSourceOverlay \
     FontRobotoOverlay \
     FontUbuntuOverlay
-
-PRODUCT_PACKAGES += \
-    ArbutusSlab-Regular.ttf \
-    Arvo-Regular.ttf \
-    Arvo-Italic.ttf \
-    Arvo-Bold.ttf \
-    Arvo-BoldItalic.ttf \
-    Barlow-Bold.ttf \
-    Barlow-Medium.ttf \
-    BigShouldersText-Bold.ttf  \
-    BigShouldersText-ExtraBold.ttf \
-    Fraunces-Regular.ttf \
-    Fraunces-SemiBold.ttf \
-    Karla-Regular.ttf \
-    Lato-Regular.ttf \
-    Lato-Italic.ttf \
-    Lato-Medium.ttf \
-    Lato-MediumItalic.ttf \
-    Lato-Bold.ttf \
-    Lato-BoldItalic.ttf \
-    Lustria-Regular.ttf \
-    Roboto-Black.ttf \
-    Roboto-BlackItalic.ttf \
-    Roboto-Bold.ttf \
-    Roboto-BoldItalic.ttf \
-    Roboto-Italic.ttf \
-    Roboto-Light.ttf \
-    Roboto-LightItalic.ttf \
-    Roboto-Medium.ttf \
-    Roboto-MediumItalic.ttf \
-    Roboto-Regular.ttf \
-    Roboto-Thin.ttf \
-    Roboto-ThinItalic.ttf \
-    Rubik-Regular.ttf \
-    Rubik-Italic.ttf \
-    Rubik-Medium.ttf \
-    Rubik-MediumItalic.ttf \
-    Rubik-Bold.ttf \
-    Rubik-BoldItalic.ttf \
-    Ubuntu-Regular.ttf \
-    Ubuntu-Italic.ttf \
-    Ubuntu-Medium.ttf \
-    Ubuntu-MediumItalic.ttf \
-    Ubuntu-Bold.ttf \
-    Ubuntu-BoldItalic.ttf \
-    Ubuntu-Condensed.ttf \
-    Ubuntu-Light.ttf \
-    Ubuntu-LightItalic.ttf \
-    Ubuntu-Thin.ttf \
-    ZillaSlab-Medium.ttf \
-    ZillaSlab-MediumItalic.ttf \
-    ZillaSlab-SemiBold.ttf \
-    ZillaSlab-SemiBoldItalic.ttf
