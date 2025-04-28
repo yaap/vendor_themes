bb945eac7a254bf7da1d69c7debd2913ec0736ce
John Galt <johngaltfirstrun@gmail.com>
2025-04-28 11:41:17 -0400

    Squash import lockscreen clockfonts
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Co-authored-by: KuroeHanako <78864678+kuroehanako@users.noreply.github.com>
    Co-authored-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- common.mk
+++ common.mk
@@ -3,3 +3,6 @@ include vendor/themes/fonts.mk
 
 # Icon Shapes
 include vendor/themes/iconshapes.mk
+
+# Lockscreen Clock Fonts
+include vendor/themes/lockscreenclockfonts.mk
---
+++ lockscreenclockfonts.mk
@@ -0,0 +1,21 @@
+# Lockscreen Clock Font overlays
+PRODUCT_PACKAGES += \
+    ClockFontAccuratistOverlay \
+    ClockFontAlphaCloudsOverlay \
+    ClockFontAlphaFlowersOverlay \
+    ClockFontAlphaWoodOverlay \
+    ClockFontCrackmanOverlay \
+    ClockFontPinewoodOverlay \
+    ClockFontNtype82Overlay \
+    ClockFontSubwayOverlay \
+    ClockFontNothingDotHeadlineOverlay \
+    ClockFontSurferOverlay \
+    ClockFontFloorlightOverlay \
+    ClockFontNeonDiscoOverlay \
+    ClockFontFibographyOverlay \
+    ClockFontPlantsLettersOverlay \
+    ClockFontRoundheadsOverlay \
+    ClockFontSpaceGameOverlay \
+    ClockFontHotSweatOverlay \
+    ClockFontIOSOverlay \
+    ClockFontCoolStoryOverlay
