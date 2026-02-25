5de049c7cf31020a37218501180c8bc57a1e4469
John Galt <johngaltfirstrun@gmail.com>
2026-02-25 17:07:37 -0500

    Revert "iconshapes: drop legacy iconshapes for now"
    This reverts commit 39fefda93b2b14dfe4d463b74cfa72597bb422c4.
    
    We are adding support for these again

--- iconshapes.mk
+++ iconshapes.mk
@@ -1,5 +1,14 @@
 # Icon shape overlays
 PRODUCT_PACKAGES += \
+    IconShapeRoundedRectOverlay \
+    IconShapeSquareOverlay \
+    IconShapeSquircleOverlay \
+    IconShapeTeardropOverlay \
+    IconShapeCylinderOverlay \
+    IconShapeHexagonOverlay \
+    IconShapePebbleOverlay \
+    IconShapeTaperedRectOverlay \
+    IconShapeVesselOverlay \
     IconShapeArchOverlay \
     IconShapeFourSidedCookieOverlay \
     IconShapeSevenSidedCookieOverlay
